ab5bdad31d1ed210db82034752ad461189312f70
Anna A. Shchetkina <morozanna2003@mail.ru>
2017-11-09 22:24:10 +0500

    Anna Shchetkina, lesson 5, Reactive Form + Template-driven Form 2

---
+++ portal/src/app/app-folders/cardlist/user-form-td/validate-bd.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ValidateBdDirective } from './validate-bd.directive';
+
+describe('ValidateBdDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ValidateBdDirective();
+    expect(directive).toBeTruthy();
+  });
+});
